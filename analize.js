86214757373819e43bfcd88f69824ab33df3ba87
Rub21 <ruben@mapbox.com>
2015-10-06 15:32:33 -0500

    filter just nodes

--- analize.js
+++ analize.js
@@ -22,20 +22,18 @@ module.exports = function(obj, id_changeset, cb) {
 			var h = val.history.reverse();
 			var p1 = turf.point([h[0].lon, h[0].lat]);
 			var p2 = turf.point([h[1].lon, h[1].lat]);
+
 			p1.properties.color = "#00f"
 			p2.properties.color = "#999"
 			var units = "kilometers";
 			//TODO, need to improve, for now th easy way.
-			var distance = (turf.distance(p1, p2, units) * 1000).toFixed(2);
-			p1.properties.distance = distance + "m";
+			var distance = (turf.distance(p1, p2, units) * 1000).toFixed(2) + "m";
+			p1.properties.distance = distance;
 			p1.properties.version = h[0].version;
 			p2.properties.version = h[1].version;
-
-			if (!_.isNaN(distance)) {
+			if (p1.geometry.coordinates[1] !== null && p2.geometry.coordinates[1] !== null) {
 				changes.features.push(p1);
 				changes.features.push(p2);
-			} else {
-				console.log(val.url);
 			}
 			//here will count how many nodes has same distances from previous version to actual version
 			if (count_distances[distance] !== undefined) {
