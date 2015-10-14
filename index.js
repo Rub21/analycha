0f4906474c62ac8129fd4fda0a942add236af134
Rub21 <ruben@mapbox.com>
2015-10-14 15:03:07 -0500

    get data

--- index.js
+++ index.js
@@ -4,19 +4,31 @@ var analize = require('./analize');
 var config = require('./config');
 var argv = require('minimist')(process.argv.slice(2));
 var async = require('async');
+var crontab = require('node-crontab');
 
-var url = config.api + 'changeset/' + argv.idchangeset + '/download';
 
-async.waterfall([
-	function(callback) {
-		changeset(url, callback);
-	},
-	function(json, callback) {
-		history(json, callback);
-	},
-	function(json, callback) {
-		analize(json, argv.idchangeset, callback)
-	}
-], function(err, result) {
-	console.log("done")
+var url = 'http://overpass-api.de/api/augmented_diff';
+
+function init() {
+	async.waterfall([
+		function(callback) {
+			changeset(url, callback);
+		},
+
+		function(json, callback) {
+			console.log(JSON.stringify(json));
+			callback();
+		}
+		// function(json, callback) {
+		// 	analize(json, argv.idchangeset, callback)
+		// }
+	], function(err, result) {
+		console.log("done")
+	});
+}
+
+
+crontab.scheduleJob("*/1 * * * *", function() { //This will call this function every 2 minutes 
+	console.log("================================================")
+	init();
 });
